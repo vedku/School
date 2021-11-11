7e1800c0b89869942004643dd43d36c78048349d
vedku <57610056+vedku@users.noreply.github.com>
2021-11-11 11:19:49 +0800

    Create task1.py

---
+++ Mock Pre-Release/task1/task1.py
@@ -0,0 +1,12 @@
+import time
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+hour_of_arrival = []
+hours = []
+frequent_parking_number = []
+day = input("What day is it?:")
+while day not in days:
+    day = input("Invalid entry! What day is it?:")
+print("ONLY INPUT THE HOUR OF ARRIVAL, EXCLUDE MINUTES. FOR EXAMPLE 15:45 becomes 15. USE 24 hour time")
+time.sleep(3)
+hour_of_arrival = int(input("What time have you arrived at?:"))
+frequent_parking_number = input("What is your frequent parking number? If you have one:")
