8859a70ebd22ed847f17909226177ed475816f47
vedku <57610056+vedku@users.noreply.github.com>
2021-11-11 11:13:59 +0800

    Update task1.py
    -change days to 1-7 and input as int so we don't need while loop error statement

--- Mock Pre-Release/task1.py
+++ Mock Pre-Release/task1.py
@@ -1,10 +1,11 @@
 import time
-days = ["monday, tuesday, wednesday, thursday, friday, saturday, sunday"]
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 hour_of_arrival = []
+hours = []
 frequent_parking_number = []
 day = input("What day is it?:")
-while day not in days
-    input("Invalid entry! What day is it?")
+while day not in days:
+    day = input("Invalid entry! What day is it?:")
 print("ONLY INPUT THE HOUR OF ARRIVAL, EXCLUDE MINUTES. FOR EXAMPLE 15:45 becomes 15. USE 24 hour time")
 time.sleep(3)
 hour_of_arrival = int(input("What time have you arrived at?:"))
