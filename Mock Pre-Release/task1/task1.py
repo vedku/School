c19fb2e83a6dfb82fe3f36e1a822f97b351e9acb
vedku <57610056+vedku@users.noreply.github.com>
2021-11-16 11:39:14 +0800

    Update task1.py

--- Mock Pre-Release/task1/task1.py
+++ Mock Pre-Release/task1/task1.py
@@ -9,8 +9,8 @@ price_per_hour = []
 print("The day of the week is represented by a number. For example, monday is 1. Accordingly answer the question using numbers ranging from 1 to 7")
 day = int(input("What day is it?:"))
 print("ONLY INPUT THE HOUR OF ARRIVAL, EXCLUDE MINUTES. FOR EXAMPLE 15:45 becomes 15. USE 24 hour time")
-#hour_of_arrival = int(input("What time have you arrived at?:"))
-#frequent_parking_number = int(input("What is your frequent parking number? If you don't have one put 0:"))
+hour_of_arrival = int(input("What time have you arrived at?:"))
+frequent_parking_number = int(input("What is your frequent parking number? If you don't have one put 0:"))
 #validation for hours parked in a day
 hours_parked = int(input("How many hours did you park for?:"))
 while day ==1 and  hour_of_arrival in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >=2:
