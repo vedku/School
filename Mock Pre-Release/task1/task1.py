a03eabd06ee9e05a7224c6a4d189507e644c796f
vedku <57610056+vedku@users.noreply.github.com>
2021-11-21 19:52:46 +0800

    Update task1.py

--- Mock Pre-Release/task1/task1.py
+++ Mock Pre-Release/task1/task1.py
@@ -14,19 +14,19 @@ time_arrived = int(input("What time have you arrived at?:"))
 
 #validation for hours parked in a day
 hours_parked = int(input("How many hours did you park for?:"))
-while day ==1 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >2:
+while day ==1 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Monday. \nHow many hours did you park for?:"))
-while day ==2 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >2:
+while day ==2 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Tuesday. \nHow many hours did you park for?:"))
-while day ==3 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >2:
+while day ==3 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Wednesday. \nHow many hours did you park for?:"))
-while day ==4 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >2:
+while day ==4 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Thursday. \nHow many hours did you park for?:"))
-while day ==5 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >2:
+while day ==5 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Friday. \nHow many hours did you park for?:"))
-while day ==6 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >4:
+while day ==6 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >4:
     hours_parked = int(input("You can't park for more than two hours on a Saturday. \nHow many hours did you park for?:"))
-while day ==7 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15] and hours_parked >8:
+while day ==7 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >8:
     hours_parked = int(input("You can't park for more than eight hours on a Sunday. \nHow many hours did you park for?:"))
 
 #parking number
@@ -47,5 +47,8 @@ if end==number:
     print("Check digit confirmed, you are eligible for a discount!")
 
 #Calculating the charges
-if time_arrived >16 and time_arrived <23 and frequent_parking_number ==1:
-    print("your mum")
+if time_arrived >16 and time_arrived <23 and end==number:
+    price = (price*0.5)
+else:
+    price = (price*0.9)
+print(price)
