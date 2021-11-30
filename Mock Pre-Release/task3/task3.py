0cff320e58d4c5e6c55a524b617d2ec20bd1f66e
vedku <57610056+vedku@users.noreply.github.com>
2021-11-30 11:47:58 +0800

    Update task3.py

--- Mock Pre-Release/task3/task3.py
+++ Mock Pre-Release/task3/task3.py
@@ -27,19 +27,19 @@ time_arrived = int(input("What time have you arrived at?:"))
 
 #validation for hours parked in a day
 hours_parked = int(input("How many hours did you park for?:"))
-while day ==1 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
+while day ==1 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Monday. \nHow many hours did you park for?:"))
-while day ==2 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
+while day ==2 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Tuesday. \nHow many hours did you park for?:"))
-while day ==3 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
+while day ==3 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Wednesday. \nHow many hours did you park for?:"))
-while day ==4 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
+while day ==4 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Thursday. \nHow many hours did you park for?:"))
-while day ==5 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
+while day ==5 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >2:
     hours_parked = int(input("You can't park for more than two hours on a Friday. \nHow many hours did you park for?:"))
-while day ==6 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >4:
+while day ==6 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >4:
     hours_parked = int(input("You can't park for more than two hours on a Saturday. \nHow many hours did you park for?:"))
-while day ==7 and  time_arrived in  [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >8:
+while day ==7 and  time_arrived in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] and hours_parked >8:
     hours_parked = int(input("You can't park for more than eight hours on a Sunday. \nHow many hours did you park for?:"))
 
 #parking number
@@ -60,20 +60,20 @@ if end == number:
     print("Check digit confirmed, you are eligible for a discount!")
 
 #calculating the charges before 4pm
-if time_arrived <=16:
-    if day == 1 and time_arrived <= 16:
+if time_arrived <= 16:
+    if day == 1:
         price_before_4 = (hours_parked*pph1)
-    elif day == 2 and time_arrived <= 16:
+    elif day == 2:
         price_before_4 = (hours_parked*pph2)
-    elif day == 3 and time_arrived <= 16:
+    elif day == 3:
         price_before_4 = (hours_parked*pph3)
-    elif day == 4 and time_arrived <= 16:
+    elif day == 4:
         price_before_4 = (hours_parked*pph4)
-    elif day == 5 and time_arrived <= 16:
+    elif day == 5:
         price_before_4 = (hours_parked*pph5)
-    elif day == 6 and time_arrived <= 16:
+    elif day == 6:
         price_before_4 = (hours_parked*pph6)
-    elif day == 7 and time_arrived <= 16:
+    elif day == 7:
         price_before_4 = (hours_parked*pph7)
     if end == number and time_arrived <= 16:
         price_before_4 = (price_before_4*0.5)
@@ -83,19 +83,19 @@ if time_arrived <=16:
 
 #calculating the charges after 4pm
 if time_arrived >= 16:
-    if day == 1 and time_arrived >= 16:
+    if day == 1:
         price_after_4 = (hours_parked*2)
-    elif day == 2 and time_arrived >= 16:
+    elif day == 2:
         price_after_4 = (hours_parked*2)
-    elif day == 3 and time_arrived >= 16:
+    elif day == 3:
         price_after_4 = (hours_parked*2)
-    elif day == 4 and time_arrived >= 16:
+    elif day == 4:
         price_after_4 = (hours_parked*2)
-    elif day == 5 and time_arrived >= 16:
+    elif day == 5:
         price_after_4 = (hours_parked*2)
-    elif day == 6 and time_arrived >= 16:
+    elif day == 6:
         price_after_4 = (hours_parked*2)
-    elif day == 7 and time_arrived >= 16:
+    elif day == 7:
         price_after_4 = (hours_parked*2)
     if end == number and time_arrived >= 16:
         price_after_4 = (price_after_4*0.5)
